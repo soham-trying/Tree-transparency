6b4ae40ccc4f6ac292141a7687ae709981ef0211
deon-gracias <deongracias1@gmail.com>
2023-04-23 02:58:15 +0530

    Used Guarded Page

--- pages/profile/index.js
+++ pages/profile/index.js
@@ -7,6 +7,7 @@ import Loading from "@/components/Loading";
 import { firestore } from "@/services/firebase.js";
 import { useUserContext } from "@/services/userContext";
 import { useUserStore } from "@/store/user";
+import GuardedPage from "@/components/GuardedPage";
 
 export default function Profile() {
   const router = useRouter();
@@ -48,52 +49,54 @@ export default function Profile() {
   return loading ? (
     <Loading />
   ) : (
-    <div className="container px-4 mx-auto">
-      <div className="shadow-2xl card">
-        <div className="card-body">
-          {/* <pre>{JSON.stringify(userStore, null, 2)}</pre> */}
-          <table className="table tabler-zebra">
-            <tbody>
-              <tr>
-                <td>Username</td>
-                <td>{userStore?.username}</td>
-              </tr>
-              <tr>
-                <td>Type</td>
-                <td>{userStore?.type}</td>
-              </tr>
-              <tr>
-                <td>Pin Code</td>
-                <td>{userStore?.pinCode}</td>
-              </tr>
-              <tr>
-                <td>Phone</td>
-                <td>{userStore?.phone}</td>
-              </tr>
-              {userStore?.type === "NGOs" ? (
-                <>
-                  <tr>
-                    <td>NGO Id</td>
-                    <td>{userStore?.ngoId}</td>
-                  </tr>
-                  <tr>
-                    <td>NGO Address</td>
-                    <td>{userStore?.ngoAddress}</td>
-                  </tr>
-                </>
-              ) : (
-                <></>
-              )}
-            </tbody>
-          </table>
+    <GuardedPage>
+      <div className="container px-4 mx-auto">
+        <div className="shadow-2xl card">
+          <div className="card-body">
+            {/* <pre>{JSON.stringify(userStore, null, 2)}</pre> */}
+            <table className="table tabler-zebra">
+              <tbody>
+                <tr>
+                  <td>Username</td>
+                  <td>{userStore?.username}</td>
+                </tr>
+                <tr>
+                  <td>Type</td>
+                  <td>{userStore?.type}</td>
+                </tr>
+                <tr>
+                  <td>Pin Code</td>
+                  <td>{userStore?.pinCode}</td>
+                </tr>
+                <tr>
+                  <td>Phone</td>
+                  <td>{userStore?.phone}</td>
+                </tr>
+                {userStore?.type === "NGOs" ? (
+                  <>
+                    <tr>
+                      <td>NGO Id</td>
+                      <td>{userStore?.ngoId}</td>
+                    </tr>
+                    <tr>
+                      <td>NGO Address</td>
+                      <td>{userStore?.ngoAddress}</td>
+                    </tr>
+                  </>
+                ) : (
+                  <></>
+                )}
+              </tbody>
+            </table>
 
-          <div className="mt-5 card-actions">
-            <Link href={"/profile/edit"} className="btn btn-secondary">
-              Edit Profile
-            </Link>
+            <div className="mt-5 card-actions">
+              <Link href={"/profile/edit"} className="btn btn-secondary">
+                Edit Profile
+              </Link>
+            </div>
           </div>
         </div>
       </div>
-    </div>
+    </GuardedPage>
   );
 }

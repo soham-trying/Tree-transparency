b1346775c127646da28f78712ca3e90e38b29fb5
deon-gracias <deongracias1@gmail.com>
2023-04-26 15:15:49 +0530

    Added rubik

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,7 +11,7 @@ module.exports = {
   theme: {
     extend: {
       fontFamily: {
-        sans: ["Poppins", ...defaultTheme.fontFamily.sans],
+        sans: ["Poppins", "Rubik", ...defaultTheme.fontFamily.sans],
       },
     },
   },

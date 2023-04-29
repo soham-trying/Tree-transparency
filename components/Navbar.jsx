176bf2eaa5726bfc70413d36db79bfe6a160523e
deon-gracias <deongracias1@gmail.com>
2023-04-29 05:40:58 +0530

    Resolved merge conflicts

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -18,8 +18,14 @@ const commonProfileOptions = [
 
 const ngoProfileOptions = [{ name: "Add Tree", href: "/tree/add" }];
 
+<<<<<<< Updated upstream
+<<<<<<< Updated upstream
 const volunteerProfileOptions = [{ name: "Verify Tree", href: "/tree/verify" }];
 
+=======
+>>>>>>> Stashed changes
+=======
+>>>>>>> Stashed changes
 export default function Navbar() {
   const router = useRouter();
 
@@ -167,6 +173,8 @@ function ProfileDropdown() {
                 </Link>
               </Menu.Item>
             ))}
+<<<<<<< Updated upstream
+<<<<<<< Updated upstream
           {userStore?.type === "Volunteers" &&
             volunteerProfileOptions.map((item) => (
               <Menu.Item>
@@ -178,6 +186,10 @@ function ProfileDropdown() {
                 </Link>
               </Menu.Item>
             ))}
+=======
+>>>>>>> Stashed changes
+=======
+>>>>>>> Stashed changes
           {commonProfileOptions.map((item) => (
             <Menu.Item>
               <Link
@@ -185,13 +197,27 @@ function ProfileDropdown() {
                 className="block px-4 py-2 duration-200 hover:bg-base-100"
               >
                 {item.name}
+<<<<<<< Updated upstream
+<<<<<<< Updated upstream
                 {item.name}
+=======
+>>>>>>> Stashed changes
+=======
+>>>>>>> Stashed changes
               </Link>
             </Menu.Item>
           ))}
           <Menu.Item>
             <div
+<<<<<<< Updated upstream
+<<<<<<< Updated upstream
               onClick={() => handleSignOut()}
+=======
+              onClick={() => signOut()}
+>>>>>>> Stashed changes
+=======
+              onClick={() => signOut()}
+>>>>>>> Stashed changes
               className="block px-4 py-2 duration-200 hover:bg-base-100"
             >
               Sign out
--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,6 @@
 import "@/styles/globals.css";
 import { UserContextProvider } from "@/services/userContext";
+<<<<<<< Updated upstream
 import Navbar from "@/components/Navbar";
 
 export default function App({ Component, pageProps }) {
@@ -11,5 +12,22 @@ export default function App({ Component, pageProps }) {
         <Component {...pageProps} />
       </main>
     </UserContextProvider>
+=======
+import { ThirdwebProvider, ChainId } from "@thirdweb-dev/react";
+
+export default function App({ Component, pageProps }) {
+  const activeChainId = ChainId.Goerli;
+
+  return (
+    <ThirdwebProvider desiredChainId={activeChainId}>
+      <UserContextProvider>
+        <Header />
+
+        <main className="h-screen">
+          <Component {...pageProps} />
+        </main>
+      </UserContextProvider>
+    </ThirdwebProvider>
+>>>>>>> Stashed changes
   );
 }

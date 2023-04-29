03f3bb83c06ef35096e76df306106c9b2b7c4055
deon-gracias <deongracias1@gmail.com>
2023-04-29 05:42:49 +0530

    Resolved merge issues

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,5 @@
 import "@/styles/globals.css";
 import { UserContextProvider } from "@/services/userContext";
-<<<<<<< Updated upstream
 import Navbar from "@/components/Navbar";
 
 export default function App({ Component, pageProps }) {
@@ -12,22 +11,5 @@ export default function App({ Component, pageProps }) {
         <Component {...pageProps} />
       </main>
     </UserContextProvider>
-=======
-import { ThirdwebProvider, ChainId } from "@thirdweb-dev/react";
-
-export default function App({ Component, pageProps }) {
-  const activeChainId = ChainId.Goerli;
-
-  return (
-    <ThirdwebProvider desiredChainId={activeChainId}>
-      <UserContextProvider>
-        <Header />
-
-        <main className="h-screen">
-          <Component {...pageProps} />
-        </main>
-      </UserContextProvider>
-    </ThirdwebProvider>
->>>>>>> Stashed changes
   );
 }
